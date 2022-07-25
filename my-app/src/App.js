d81a5eb45ed45c5749fa541593261b126bb6b0f6
Anwar Hossain <mdanwarhossain1996bd@gmail.com>
2022-07-25 11:12:40 +0600

    Navbar complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import PreNavbar from './components/PreNavbar';
+import Navbar from "./components/Navbar.js";
+import {BrowserRouter as Router} from "react-router-dom";
+import Slider from "./components/Slider.js"
 
 function App() {
   return (
-    <div>
-
-    <PreNavbar />
-     
-    </div>
+    <Router>
+      <PreNavbar />
+      <Navbar />
+      <Slider />
+    </Router>
   );
 }
 
