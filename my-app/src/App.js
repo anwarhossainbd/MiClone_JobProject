407acdc55a831a291193bdacfdb39f3afcd1e7d5
Anwar Hossain <mdanwarhossain1996bd@gmail.com>
2022-07-29 11:33:34 +0600

    Slider Done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,14 @@ import PreNavbar from './components/PreNavbar';
 import Navbar from "./components/Navbar.js";
 import {BrowserRouter as Router} from "react-router-dom";
 import Slider from "./components/Slider.js"
+import data from "./data/data.json"
 
 function App() {
   return (
     <Router>
       <PreNavbar />
       <Navbar />
-      <Slider />
+      <Slider start={data.banner.start} />
     </Router>
   );
 }
--- my-app/src/components/Slider.js
+++ my-app/src/components/Slider.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel'
 
-const Slider = () => {
+const Slider = ({start}) => {
   return (
-    <div>Slider</div>
+     <Carousel fade>
+
+       {start.map((item)=>(
+          <Carousel.Item>
+             <img className="d-block w-100" src={item} alt="First slide" />
+          </Carousel.Item>
+       ))}
+
+    </Carousel>
   )
 }
 
